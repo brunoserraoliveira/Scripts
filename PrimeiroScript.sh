90ad717d4186c4c4240c41e14f918d137a6075b0
Bruno <brunoserrasilva@gmail.com>
2024-03-24 17:47:36 -0300

    Add comandon date e sort

--- PrimeiroScript.sh
+++ PrimeiroScript.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 
-echo "========================== Meu primeiro script =========================="
+echo "==================== Meu primeiro script ====================="
+echo ""
+echo -n "Exibir data e hora atual"
+date
+echo "=============================================================="
+echo "Listagem dos alunos"
+sort ../arquivos/alunos2.txt
 
 
